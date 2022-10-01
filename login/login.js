b8aa28c584283ee57f3136d37282fe8d7662bba4
Alex <alex11737@gmail.com>
2022-10-01 22:07:07 +0300

    added feature to hide menu when user clicks out

--- login/login.js
+++ login/login.js
@@ -64,3 +64,13 @@ function closeNav() {
   document.getElementById("sidenav").style.width = "0";
   document.getElementById("sidenav-icon").style.visibility = "visible";
 }
+
+function handleMousePos(event) {
+      var mouseClickWidth = event.clientX;
+      if(mouseClickWidth>=300){
+        document.getElementById("sidenav").style.width = "0";
+        document.getElementById("sidenav-icon").style.visibility = "visible";
+      }
+}
+
+document.addEventListener("click", handleMousePos);
